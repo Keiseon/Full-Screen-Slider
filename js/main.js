59dc5085148f7d27f8ba9e7e3a20cfc18a3c4b0c
keiseon <keiseonhamilton@gmai.com>
2019-08-18 03:00:42 -0400

    Completed the animation for .content div

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const slides = document.querySelectorAll(".slide");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
-const auto = false; // Set this true to auto run slide
-const intervalTime = 3000;
+const content = document.querySelectorAll(".content");
+const auto = true; // Set this true to auto run slide
+const intervalTime = 6000;
 let slideInterval;
 
 // Arrow function for Next
@@ -10,17 +11,23 @@ let slideInterval;
 const nextSlide = () => {
   // Get the current class
   const current = document.querySelector(".current");
+  const slideRightAnime = document.querySelector(".slide-right-anime");
 
   // Remove the current class
   current.classList.remove("current");
+  slideRightAnime.classList.remove("slide-right-anime");
 
   //   Check for next slide
   if (current.nextElementSibling) {
     //Add the ".current" class to the next slide sibling
     current.nextElementSibling.classList.add("current");
+    current.nextElementSibling.firstElementChild.classList.add(
+      "slide-right-anime"
+    );
   } else {
     // when you reach the end of the list of slides add the ".current" class back to the start
     slides[0].classList.add("current");
+    content[0].classList.add("slide-right-anime");
   }
 
   //Delay then Remove the current class again
@@ -32,21 +39,28 @@ const nextSlide = () => {
 const prevSlide = () => {
   // Get the current class
   const current = document.querySelector(".current");
+  const slideRightAnime = document.querySelector(".slide-right-anime");
 
   // Remove the current class
   current.classList.remove("current");
+  slideRightAnime.classList.remove("slide-right-anime");
 
   //   Check for Previous slide
   if (current.previousElementSibling) {
     //Add the ".current" class to the Previous slide sibling
     current.previousElementSibling.classList.add("current");
+    current.previousElementSibling.firstElementChild.classList.add(
+      "slide-right-anime"
+    );
   } else {
     // when you reach the beginning of the list of slides add the ".current" class back to the end sibling
     slides[slides.length - 1].classList.add("current");
+    content[content.length - 1].classList.add("slide-right-anime");
   }
 
   //Delay then Remove the current class again
   setTimeout(() => current.classList.remove("current"));
+  setTimeout(() => slideRightAnime.classList.remove("slide-right-anime"));
 };
 
 // Button Events that call your functions
